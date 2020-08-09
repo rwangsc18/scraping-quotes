db72bfb32364440b718abb049e15a48084853760
Rui Wang <rwangsc18@gmail.com>
2020-08-08 17:51:24 -0700

    add comment

--- locators/quotes_page_locators.py
+++ locators/quotes_page_locators.py
@@ -1,3 +1,4 @@
 # locator to extract a quote from a page
+# constant in python typically use all capital letters
 class QuotePageLocators:
-    QUOTE = 'div.quote'
+    QUOTE = 'div.col-md-8 div.quote'
